3d5a5d67c84877fa67c795bbcce9aef0d186bb9d
Stefan Weixelbaumer <sweixelbaumer@apryse.com>
2025-03-28 08:22:13 +0000

    Remove ngrok references and clean up URL handling for Cloudflare tunnel

---
+++ config.js
@@ -0,0 +1,7 @@
+// Remove any ngrok-related config
+const config = {
+    port: process.env.PORT || 3000,
+    baseUrl: process.env.BASE_URL || 'https://dev.tandav.com'
+};
+
+module.exports = config; 
---
+++ start-dev-tunnel.sh
@@ -0,0 +1,1 @@
+cloudflared tunnel --config /Users/weix/.cloudflared/config.yml run github-fetcher-dev
